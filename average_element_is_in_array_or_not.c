7655981bb827e2077df6ce07d46ed9d2864368dd
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-04-21 19:58:11 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,avg,s=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=s+arr[i];     }     avg=s/n;     for(i=0;i<n;i++)     {         if(avg==arr[i])         {             c=1;             break;         }     }     if(c==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ average_element_is_in_array_or_not.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,avg,s=0,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+arr[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(avg==arr[i])
+        {
+            c=1;
+            break;
+        }
+    }
+    if(c==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
