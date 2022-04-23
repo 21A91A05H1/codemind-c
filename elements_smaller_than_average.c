68cf6d71d83ac731ceffc42ae11416b05be79a6c
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-04-23 20:17:15 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],avg,s=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=s+arr[i];     }     avg=s/n;     for(i=0;i<n;i++)     {         if(avg>=arr[i])         {             c++;         }     }     printf("%d",c); }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],avg,s=0,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+arr[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(avg>=arr[i])
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
