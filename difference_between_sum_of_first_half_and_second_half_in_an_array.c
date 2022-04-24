cc2dbca80291a5721323cf954a317718d1cad5f0
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-04-24 10:31:04 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,arr[100],i,s1=0,s2=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i>=n/2)         {             s2=s2+arr[i];         }         else         {             s1=s1+arr[i];         }     }     printf("%d",abs(s1-s2)); }

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,arr[100],i,s1=0,s2=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i>=n/2)
+        {
+            s2=s2+arr[i];
+        }
+        else
+        {
+            s1=s1+arr[i];
+        }
+    }
+    printf("%d",abs(s1-s2));
+}
