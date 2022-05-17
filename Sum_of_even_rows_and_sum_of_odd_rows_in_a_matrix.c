6e889c6e55729afd31cdeed1291d34a58c26bb39
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-05-17 19:55:55 +0530

    #include<stdio.h> int main() {     int c,r,i,j,a[100][100],os=0,es=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(i%2==0)             {                 es=es+a[i][j];             }             else             {                 os=os+a[i][j];             }         }     }     printf("%d %d",es,os); }

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int c,r,i,j,a[100][100],os=0,es=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i%2==0)
+            {
+                es=es+a[i][j];
+            }
+            else
+            {
+                os=os+a[i][j];
+            }
+        }
+    }
+    printf("%d %d",es,os);
+}
