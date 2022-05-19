e6e30481af21e5fb00ff4b590499793d68a8802a
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-05-19 18:38:28 +0530

    #include<stdio.h> int main() {     int a[3],b[3],i,as=0,bs=0;     for(i=0;i<3;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<3;i++)     {         scanf("%d",&b[i]);     }     for(i=0;i<3;i++)     {         if(a[i]>b[i])         {             as++;         }         if(a[i]<b[i])         {             bs++;         }     }     printf("%d %d",as,bs); }

---
+++ Compare_Triplets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[3],b[3],i,as=0,bs=0;
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<3;i++)
+    {
+        if(a[i]>b[i])
+        {
+            as++;
+        }
+        if(a[i]<b[i])
+        {
+            bs++;
+        }
+    }
+    printf("%d %d",as,bs);
+}
