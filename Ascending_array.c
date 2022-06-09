55d7a65e5a557a497d1917eee225a3f6e11f6c66
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-06-09 13:14:50 +0530

    #include<stdio.h> int main() {     int a[100],i,j,n,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {        for(j=i+1;j<n;j++)        {            if(a[i]>=a[j])            {                flag=1;                break;            }        }     }     if(flag==0)     {         printf("yes");     }     else     {         printf("no");     } }

---
+++ Ascending_array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,j,n,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       for(j=i+1;j<n;j++)
+       {
+           if(a[i]>=a[j])
+           {
+               flag=1;
+               break;
+           }
+       }
+    }
+    if(flag==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
