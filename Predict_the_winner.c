aae30506050564ba4658f562ba854f1576410f0b
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-07-17 09:31:43 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,arr[100],x=0,y=0,m;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             x=x+arr[i];         }         else         {             y=y+arr[i];         }     }    m=abs(x-y);    if(m%4==0)    {        printf("X");    }    else    {        printf("Y");    } }

---
+++ Predict_the_winner.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,arr[100],x=0,y=0,m;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            x=x+arr[i];
+        }
+        else
+        {
+            y=y+arr[i];
+        }
+    }
+   m=abs(x-y);
+   if(m%4==0)
+   {
+       printf("X");
+   }
+   else
+   {
+       printf("Y");
+   }
+}
