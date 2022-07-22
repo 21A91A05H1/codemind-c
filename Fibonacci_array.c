c323149ab3bde4da983555fc88fe1f416a1a98cc
21A91A05H1 <97424381+21A91A05H1@users.noreply.github.com>
2022-07-22 11:17:38 +0530

    #include<stdio.h> int main() {     int i,arr[100],f=0,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<n-1;i++)     {         if(arr[i]+arr[i-1]==arr[i+1])         {             f=1;         }         else         {             f=0;             break;         }     }     if(f==0)     {         printf("no");     }     else     {         printf("yes");     } }

---
+++ Fibonacci_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,arr[100],f=0,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n-1;i++)
+    {
+        if(arr[i]+arr[i-1]==arr[i+1])
+        {
+            f=1;
+        }
+        else
+        {
+            f=0;
+            break;
+        }
+    }
+    if(f==0)
+    {
+        printf("no");
+    }
+    else
+    {
+        printf("yes");
+    }
+}
